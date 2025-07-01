622f2f67c011b8c3b7973bb4c34c86992b0fa192
telesyk <telesyk89@gmail.com>
2025-07-01 10:02:06 +0200

    'use-memo' Minor cleanup

--- use-memo-use-callback/src/components/CarItem.jsx
+++ use-memo-use-callback/src/components/CarItem.jsx
@@ -1,11 +1,5 @@
 export default function CarItem({ car }) {
   return (
-    // <div className="join-item flex gap-2 py-3 border-b-2">
-    //   <span className="font-bold">{car.name}</span>
-    //   <span className="">{car.model}</span>
-    //   <span className="">{car.year_produced}</span>
-    //   <span className="flex-auto text-end">$ {car.price}</span>
-    // </div>
     <div className="card card-border border-accent w-96 shadow-sm ">
       <div className="card-body">
         <span className="badge badge-md badge-success">{car.name}</span>
